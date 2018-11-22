9544596464441fc54a34a580f8f5bc9fc4570586
stephenlasky <stephenjlasky@gmail.com>
2018-11-21 19:50:39 -0500

    NO MESSAGE

--- adder.py
+++ adder.py
@@ -82,7 +82,7 @@ def adjust_learning_rate(optimizer, epoch):
         # MIN = 0.001
         # if lr < MIN:
         #     lr = MIN
-        if int(NUM_EPOCHS / 10) % epoch == 0 and epoch != 0:
+        if epoch != 0 and int(NUM_EPOCHS / 10) % epoch == 0:
             lr *= 0.5
             print("learning rate:",lr)
         param_group['lr'] = lr
